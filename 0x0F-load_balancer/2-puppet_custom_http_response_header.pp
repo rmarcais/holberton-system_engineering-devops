69c79957dc66abe4bea690a68be6885c0bae6950
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-25 15:42:36 +0200

    Fix bugs

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,19 +6,6 @@ ensure   => 'installed',
 provider => 'apt',
 }
 
-file { '/var/www/html/index.nginx-debian.html':
-ensure  => 'present',
-path    => '/var/www/html/index.nginx-debian.html',
-content => 'Hello World\n',
-}
-
-file_line { 'Add the redirect_me page':
-ensure => 'present',
-path   => '/etc/nginx/sites-available/default',
-after  => 'server_name _;',
-line   => 'location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; }',
-}
-
 file_line { 'Add the custom HTTP header':
 ensure => 'present',
 path   => '/etc/nginx/sites-available/default',
