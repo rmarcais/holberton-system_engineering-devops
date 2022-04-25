d73a02cd32f84f5ec52916827e93d284210dff81
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-25 16:39:08 +0200

    Get back to the previous version

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,22 @@
 # Puppet manifest containing commands to automatically
 # configure an Ubuntu machine
 
+exec { 'update':
+command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-ensure => present,
-name   => 'nginx',
+ensure  => present,
+name    => 'nginx',
+require => Exec['update'],
 }
 
 file_line { 'Add header':
-ensure => 'present',
-path   => '/etc/nginx/sites-available/default',
-after  => 'server_name _;',
-line   => 'add_header X-Served-By $hostname;',
+ensure  => 'present',
+path    => '/etc/nginx/sites-available/default',
+after   => 'server_name _;',
+line    => 'add_header X-Served-By $hostname;',
+require => Package['nginx'],
 }
 
 service { 'nginx':
