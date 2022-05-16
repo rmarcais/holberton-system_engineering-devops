e36719569041f8605ffc1ae9dbdea17f000bd6dd
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-16 16:12:16 +0200

    Add new version

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,7 +2,7 @@
 """
 Python script that exports data using the CSV format.
 Records all tasks from all employees
-"""
+
 
 import json
 import requests
@@ -24,6 +24,31 @@ if __name__ == "__main__":
                     "completed": value.get("completed")}
             todo_list.append(dico)
         dictionary[uid] = todo_list
-    json_object = json.dumps(dictionary)
-    with open(filename, "a") as f:
-        f.write(json_object)
+    with open(filename, "w") as f:
+        json.dump(dictionary, f)"""
+"""Exports data in the JSON format"""
+
+if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
