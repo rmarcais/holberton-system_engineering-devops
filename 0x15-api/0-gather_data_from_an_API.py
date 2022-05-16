d3a2ad3b92346f7d6e477b412cd8534ecdaca087
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-16 13:26:39 +0200

    Add 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script that, using the REST API, for a given enmployee ID,
+returns information about his/her TODO list progress.
+"""
+
+
+import requests
+from sys import argv
+
+url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+api_url = requests.get(url)
+username = api_url.json().get("name")
+
+api_url_todo = requests.get("{}/todos".format(url))
+completed = []
+total = len(api_url_todo.json())
+
+for value in api_url_todo.json():
+    if value.get("completed") is True:
+        completed.append(value.get("title"))
+
+print("Employee {} is done with tasks ({}/{})"
+      .format(username, len(completed), total))
+for i in completed:
+    print("\t {}".format(i))
